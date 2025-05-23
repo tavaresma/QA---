a9a9b86ace9c5b90b3f9642f94ff0ff0a869dc05
Maria Aparecida Tavares da Silva <158099088+tavaresma@users.noreply.github.com>
2025-05-23 09:41:10 -0300

    Create regressao.py

---
+++ regressao.py
@@ -0,0 +1,7 @@
+def test_regressao_login():
+    assert login_usuario("marimar@gmail.com", "4780") == "Login bem-sucedido"
+    print("Teste de regressão para o login passou!")
+
+test_regressao_login()
+
+Saida: Teste de regressão para o login passou!
